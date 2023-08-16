b7753edf3575f3f3bf0c2e574d504f651f8ca244
s94wanniarachchige <132711913+shiwantha-memaree@users.noreply.github.com>
2023-08-16 13:04:48 -0400

    drawer changes

--- JobHunt/src/Components/Navigation/CustomDrawer.js
+++ JobHunt/src/Components/Navigation/CustomDrawer.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity,Image } from 'react-native';
 import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
 
 const CustomDrawer = ({navigation}) => {
-
+const [userName,setUserName] = useState("Jhon Doe")
+const [email,setEmail] =useState("jhoneDoe@example.com")
+const [profileType, setProfileType] = useState("Recruiter");
 return(
 <DrawerContentScrollView>
 <TouchableOpacity
@@ -12,10 +14,31 @@ return(
         onPress={() => navigation.closeDrawer()} // Close the drawer when header is clicked
       >
           <Ionicons name="menu-outline" size={24} color="black" style={{ marginRight: 10 }} />
-        <Text style={{ fontSize: 18 }}>Menu</Text>
-        <DrawerItem label="Profile" onPress={() => navigation.navigate('./')} />
-
+        <Text style={{ fontSize: 18 }}>Profile</Text>
       </TouchableOpacity>
+<View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', marginVertical: 2,marginLeft:10 }}>
+  <Image
+    source={require("../../../assets/default_profile_avatar.png")}
+    style={{
+      width: 120,
+      height: 120,
+      borderRadius: 60,
+    }}
+  />
+  <TouchableOpacity
+    onPress={() => { alert("clicked")}}
+    style={{
+      position: 'absolute',
+      bottom: 0,
+      left:80,
+      backgroundColor: 'white',
+      borderRadius: 15,
+      padding: 5,
+    }}
+  >
+    <Ionicons name="pencil-outline" size={24} color="black" />
+  </TouchableOpacity>
+</View>
 </DrawerContentScrollView>
 
 );
