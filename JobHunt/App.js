1c7901fd731891cd109cb6d61c7b1eb9d2e0ba36
s-shaligram <s_shaligram@fanshaweonline.ca>
2023-08-19 18:48:17 -0400

    initial code splash screen

--- JobHunt/App.js
+++ JobHunt/App.js
@@ -2,12 +2,48 @@ import LoginScreen from "./src/Components/Screens/loginScreen";
 import {NavigationContainer} from "@react-navigation/native";
 import ReactNavigationBottomTabs from "./src/Components/Navigation/tab";
 import { StateProvider, useStateContext} from "./src/context/StateContext";
+import * as SplashScreen from "expo-splash-screen";
+import { useState, useEffect, useCallback } from "react";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+
+    const [appIsReady, setAppIsReady] = useState(false);
+
+useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare().then();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
     return (
+        <View onLayout={onLayoutRootView}>
         <StateProvider>
             <AppNavigator />
         </StateProvider>
+        </View>
     );
 }
 function AppNavigator() {
@@ -18,6 +54,7 @@ function AppNavigator() {
     }
 
     return (
+        
         <NavigationContainer>
             {authenticatedUser ? <ReactNavigationBottomTabs /> : <LoginScreen />}
         </NavigationContainer>
