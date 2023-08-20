3afdb80b05b6b07fd8399682231037e80079ad5b
Kavin Ranawella <kavin@memaree.com>
2023-08-19 20:52:28 -0400

    adding data mapping for recruiter

--- JobHunt/src/Components/Screens/HomeScreen/PostingDetails.js
+++ JobHunt/src/Components/Screens/HomeScreen/PostingDetails.js
@@ -1,15 +1,17 @@
-import {Button, ScrollView, StyleSheet, Text, View} from "react-native";
+import {Button, ScrollView, Text, View} from "react-native";
 import {Card} from "react-native-elements";
 import React from "react";
+import {useStateContext} from "../../../context/StateContext";
 
 export const PostingDetails = ({route, navigation}) => {
     const { details } = route.params;
+    const {authenticatedUser} = useStateContext();
 
     return (
         <ScrollView>
             <View>
                 <Card containerStyle={{marginBottom: 15}}>
-                    <View style={{marginBottom: 20}}><Button onPress={() => navigation.goBack()} title={"Apply"}></Button></View>
+                    {authenticatedUser.userType !== 'Recruiter' && <View style={{marginBottom: 20}}><Button onPress={() => navigation.goBack()} title={"Apply"}></Button></View>}
                     <Text style={{fontSize: 25, fontWeight: 'bold'}}>{details?.jobTitle}</Text>
                     <Text style={{fontWeight: 'bold'}}>{details?.companyName}</Text>
                     <Text style={{fontStyle: 'italic'}}>{details?.location}</Text>
@@ -18,7 +20,8 @@ export const PostingDetails = ({route, navigation}) => {
                     <Text style={{marginTop: 20}}>{details?.jobDescription}</Text>
                     <Text style={{marginTop: 20, fontWeight: 'bold'}}>Contact Details:</Text>
                     <Text>{details?.contactDetails}</Text>
-                    <View style={{marginTop: 20}}><Button onPress={() => navigation.goBack()} title={"Apply"}></Button></View>
+                    <Text>{details?.postedBy}</Text>
+                    {authenticatedUser.userType !== 'Recruiter' && <View style={{marginTop: 20}}><Button onPress={() => navigation.goBack()} title={"Apply"}></Button></View>}
                 </Card>
             </View>
         </ScrollView>
